28625bea89434291e8d5fe70f4da74591b77ebfd
cmbsolver <cmbsolver@gmail.com>
2024-11-01 22:43:26 -0500

    Adding mediator and six labors.

--- Cart.Util/Program.cs
+++ Cart.Util/Program.cs
@@ -11,6 +11,7 @@ public class Program
         var builder = WebAssemblyHostBuilder.CreateDefault(args);
         builder.RootComponents.Add<App>("#app");
         builder.RootComponents.Add<HeadOutlet>("head::after");
+        builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
         builder.Services.AddMudServices();
         builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
